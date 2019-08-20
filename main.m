fcfe6adfd2f8af69964f860df3cae1d33534e703
Oscar Rigoberto Carvajal Ortiz <or.carvajal.ortiz@gmail.com>
2019-08-20 13:14:03 -0700

    cambiando a 930 evaluaciones

--- main.m
+++ main.m
@@ -1,39 +1,3 @@
-%___________________________________________________________________%
-%  Ant Lion Optimizer (ALO) source codes demo version 1.0           %
-%                                                                   %
-%  Developed in MATLAB R2011b(7.13)                                 %
-%                                                                   %
-%  Author and programmer: Seyedali Mirjalili                        %
-%                                                                   %
-%         e-Mail: ali.mirjalili@gmail.com                           %
-%                 seyedali.mirjalili@griffithuni.edu.au             %
-%                                                                   %
-%       Homepage: http://www.alimirjalili.com                       %
-%                                                                   %
-%   Main paper:                                                     %
-%                                                                   %
-%   S. Mirjalili, The Ant Lion Optimizer                            %
-%   Advances in Engineering Software , in press,2015                %
-%   DOI: http://dx.doi.org/10.1016/j.advengsoft.2015.01.010         %
-%                                                                   %
-%___________________________________________________________________%
-
-% You can simply define your cost in a seperate file and load its handle to fobj 
-% The initial parameters that you need are:
-%__________________________________________
-% fobj = @YourCostFunction
-% dim = number of your variables
-% Max_iteration = maximum number of generations
-% SearchAgents_no = number of search agents
-% lb=[lb1,lb2,...,lbn] where lbn is the lower bound of variable n
-% ub=[ub1,ub2,...,ubn] where ubn is the upper bound of variable n
-% If all the variables have equal lower bound you can just
-% define lb and ub as two single number numbers
-
-% To run ALO: [Best_score,Best_pos,cg_curve]=ALO(SearchAgents_no,Max_iteration,lb,ub,dim,fobj)
-%__________________________________________
-
-
 clear all 
 clc
 warning off
@@ -47,23 +11,23 @@ for k1=1:experiments
 % if nargin<1
 %    para=[50 0.8];
 % end
-tic;
-s1=pwd; %Identify current folder
-s2=['\erroresALO' num2str(k1) '.txt'];
+% tic;
+% s1=pwd; %Identify current folder
+% s2=['\erroresALO' num2str(k1) '.txt'];
 %$s2='\erroresFPA0810.txt';
-dir = strcat(s1,s2);
+% dir = strcat(s1,s2);
 %--crear arhivo para guardar errores
-error1= fopen(dir, 'wt');
+% error1= fopen(dir, 'wt');
 tic;
-bajo = 10;
-alto = 30;
-SearchAgents_no = round(rand(1,experiments).*(alto-bajo)+bajo);
-% SearchAgents_no=[36 38 40 42 44 46 48 50 52 54 56 58 60 62 64 66 68 70 72 74 76 78 80];           % Population size, typically 10 to 25
+% bajo = 10;
+% alto = 30;
+% SearchAgents_no = round(rand(1,experiments).*(alto-bajo)+bajo);
+SearchAgents_no=[18 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50 52 54 56 58 60 62 64 66 68 70 72 74 76];           % Population size, typically 10 to 25
 % Iteration parameters
-bajo = 150;
-alto = 200;
-Max_iteration = round(rand(1,experiments).*(alto-bajo)+bajo);
-% Max_iteration=[278 263 250 238 227 217 208 200 192 185 179 172 167 161 156];            % Total number of iterations
+% bajo = 150;
+% alto = 200;
+% Max_iteration = round(rand(1,experiments).*(alto-bajo)+bajo);
+Max_iteration=[52 47 42 39 36 33 31 29 27 26 24 23 22 21 20 19 19 18 17 17 16 16 15 15 14 14 13 13 13 12];            % Total number of iterations
 
 
 SearchAgents_no=SearchAgents_no(k1);
@@ -84,15 +48,15 @@ arquitec(k1).Max_iteration = Max_iteration;
 arquitec(k1).SearchAgents_no = SearchAgents_no;
 arquitec(k1).tiempo = tiempo;
 arquitec(k1).bestnnall = bestnnall;
-save('redes3.mat','arquitec');
+save('redes930_3.mat','arquitec');
 
 display(['The best solution obtained by ALO is : ', num2str(Best_pos)]);
 display(['The best optimal value of the objective funciton found by ALO is : ', num2str(Best_score)]);
 
-
-fprintf('El proceso ha tardado %d minutos \n', tiempo);
-fprintf('El proceso ha tardado %d segundos ', tiempos);
-fprintf(error1,['Best Solution:' num2str(Best_pos) ' fmin=',num2str(Best_score) '   ALO time minutos:', num2str(tiempo) '   Experimento:', num2str(k1) '   SearchAgents_no:', num2str(SearchAgents_no)  '   Max_iteration:', num2str(Max_iteration) '\n']);        
+% 
+% fprintf('El proceso ha tardado %d minutos \n', tiempo);
+% fprintf('El proceso ha tardado %d segundos ', tiempos);
+% fprintf(error1,['Best Solution:' num2str(Best_pos) ' fmin=',num2str(Best_score) '   ALO time minutos:', num2str(tiempo) '   Experimento:', num2str(k1) '   SearchAgents_no:', num2str(SearchAgents_no)  '   Max_iteration:', num2str(Max_iteration) '\n']);        
 
 end
 
